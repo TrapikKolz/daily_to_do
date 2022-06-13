463eaf23d11ad13129d83b3b691f15b09f7a56a9
TrapikKolz <striffemen@gmail.com>
2022-06-13 12:41:34 +0300

    Добавили функцию отображения конкретной записи, изменения ее и удаления через DRF

--- src/daily_to_do/api/views.py
+++ src/daily_to_do/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from ..api.serializers import NoteSerializer
 from ..notes.models import Note
 
-@api_view(['GET','Post'])
+@api_view(['GET','POST'])
 def notes_list(request):
     if request.method == 'GET':
         notes = Note.objects.all()
@@ -18,4 +18,21 @@ def notes_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
+@api_view(['GET','PUT','DELETE'])
+def notes_list(request):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
