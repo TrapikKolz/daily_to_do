3e657c3cf8866c09d8c324cb99cafc96389a549c
TrapikKolz <striffemen@gmail.com>
2022-06-13 12:21:09 +0300

    Добавили функцию отображения всех записей и добавление новой записи через DRF

--- src/daily_to_do/api/views.py
+++ src/daily_to_do/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from ..api.serializers import NoteSerializer
+from ..notes.models import Note
+
+@api_view(['GET','Post'])
+def notes_list(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = NoteSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
